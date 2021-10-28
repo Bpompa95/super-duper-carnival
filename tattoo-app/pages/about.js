9206e006da9c6eef1bf41aff7c8a5968628b4eed
Brian Pompa <brianpompa02@gmail.com>
2021-10-27 22:26:01 -0700

    fill out about, index and profile pages

--- tattoo-app/pages/about.js
+++ tattoo-app/pages/about.js
@@ -3,7 +3,13 @@ import React from 'react'
 const about = () => {
     return (
         <div>
-            
+            <h2>Welcome to Tattoo locator, 
+                where you can find a tattoo shop near you,
+                 see the artist arts, his/her profile, 
+                 choose your own appointments,
+                  communicate with the art you chosen. 
+                  It is not just for tattoos you can also,
+                   find someone with piercing experiments.</h2>
         </div>
     )
 }
